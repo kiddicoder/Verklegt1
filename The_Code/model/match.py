3e72dda0ca30ecd852cf3e7946e133488bf03d8a
joninam <118810664+joninam@users.noreply.github.com>
2022-11-29 13:37:49 +0000

    Update match.py

--- The_Code/model/match.py
+++ The_Code/model/match.py
@@ -2,7 +2,7 @@ from model.team import Team
 from model.player import Player
 
 class Match():
-    def __init__(self, home_team="" away_team="", home_player="", away_player="", results="", date_of_match="", new_date_of_match=""):
+    def __init__(self, home_team="", away_team="", home_player="", away_player="", results="", date_of_match="", new_date_of_match=""):
         self.home_team = Team().id()
         self.away_team = Team().id()
         self.home_player = Player().id()
