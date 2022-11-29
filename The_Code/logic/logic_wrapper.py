273307bdc6431409b60a2b2312305e83ce9771f8
elsaruno <118810416+elsaruno@users.noreply.github.com>
2022-11-29 13:31:19 +0000

    Update logic_wrapper.py

--- The_Code/logic/logic_wrapper.py
+++ The_Code/logic/logic_wrapper.py
@@ -1,1 +1,21 @@
+from logic.club_logic import ClubLogic
+from logic.match_logic import MatchLogic
+from logic.player_logic import PlayerLogic
+from logic.team_logic import TeamLogic
+from logic.tournament_logic import TournamentLogic
+from data.data_wrapper import DataWrapper
 
+class LogicWrapper:
+    def __init__(self):
+        self.data_wrapper = DataWrapper()
+        self.club_logic = ClubLogic()
+        self.match_logic = MatchLogic()
+        self.player_logic = PlayerLogic()
+        self.team_logic = TeamLogic()
+        self.tournament_logic = TournamentLogic()
+    
+    def create_player(self, customer):
+        return self.player_logic.create_player(customer)
+
+    def list_all_players(self):
+        return self.player_logic.list_all_players()
