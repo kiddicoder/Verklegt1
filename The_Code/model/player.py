a0a57cf2a918018e4d1e0caef2e175f1936cd00c
alexiaaxels <112617700+alexiaaxels@users.noreply.github.com>
2022-11-29 11:41:42 +0000

    Update player.py

--- The_Code/model/player.py
+++ The_Code/model/player.py
@@ -1,1 +1,14 @@
+class Player(object):
+   def __init__(self, name="", id=0, address="", email="", gsm=0, phone=0, role="", team=""):
+       self.name = name
+       self.id = id
+       self.address = address
+       self.email = email
+       self.gsm = gsm
+       self.phone = phone
+       self.role = role
+       self.team = team
+  
+   def __str__(self):
+       return f"{self.name}, {self.id}, {self.address}, {self.email}, {self.gsm}, {self.phone}, {self.role}, {self.team}"
 
