7de27a59b9bba4ee902cce687e1d4ad5180c3b03
elsaruno <118810416+elsaruno@users.noreply.github.com>
2022-11-29 13:27:50 +0000

    Update player_logic.py

--- The_Code/logic/player_logic.py
+++ The_Code/logic/player_logic.py
@@ -1,1 +1,13 @@
+from data.player_data import PlayerData
+from model.player import Player
+
+class PlayerLogic:
+    def __init__(self, data_connection):
+        self.data_wrapper = data_connection
+
+    def create_player(self, player):
+        self.data_wrapper.create_player(player)
+
+    def list_all_players(self):
+        return self.data_wrapper.list_all_players()
 
