02d4c177462a5aaa1b6d65e1abe4ca014f411267
elsaruno <118810416+elsaruno@users.noreply.github.com>
2022-11-29 13:31:54 +0000

    Update player_data.py

--- The_Code/data/player_data.py
+++ The_Code/data/player_data.py
@@ -1,1 +1,13 @@
+import csv
+from model.player import Player
 
+class PlayerData:
+    def __init__(self):
+        self.file_name = 'files/players.csv'
+
+    def create_player(self, player):
+        with open(self.file_name, 'a', newline='', encoding = 'utf-8') as csvfiles:
+            fieldnames = ['name', 'id', 'address', 'gsm', 'phone', 'email', 'role', 'team']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writerow({'name': player.name, 'id': player.id, 'address': player.address, 'gsm': player.gsm, 'phone': player.phone, 'email': player.email, 'role': player.role, 'team': player.team})
