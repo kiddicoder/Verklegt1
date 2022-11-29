a0b5d54a14cded41f5c362e2f32a2bfc929ffa72
elsaruno <118810416+elsaruno@users.noreply.github.com>
2022-11-29 13:42:58 +0000

    Update player_data.py

--- The_Code/data/player_data.py
+++ The_Code/data/player_data.py
@@ -11,3 +11,11 @@ class PlayerData:
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
             writer.writerow({'name': player.name, 'id': player.id, 'address': player.address, 'gsm': player.gsm, 'phone': player.phone, 'email': player.email, 'role': player.role, 'team': player.team})
+
+    def read_all_players(self):
+        return_list = []
+        with open(self.file_name, newline='', encoding = 'utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                return_list.append(Player(row['name'], row['id'], row['address'], row['gsm'], row['phone'], row['email'], row['role'], row['team']))
+        return return_list
