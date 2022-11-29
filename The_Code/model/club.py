d389791df40b63f3df2bcce0dfd54613bdd855e0
joninam <118810664+joninam@users.noreply.github.com>
2022-11-29 13:40:12 +0000

    Update club.py

--- The_Code/model/club.py
+++ The_Code/model/club.py
@@ -1,4 +1,5 @@
 from model.player import Player
+from model.team import Team
 class Club():
     def __init__(self, club_name="", club_address="", club_phone=0, team=""):
         self.club_name = club_name
