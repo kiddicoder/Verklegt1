8b4320609403b4bb2c7a5c23878cf4fb23451d12
joninam <118810664+joninam@users.noreply.github.com>
2022-11-30 12:03:57 +0000

    Update player_data.py

--- The_Code/data/player_data.py
+++ The_Code/data/player_data.py
@@ -8,7 +8,7 @@ class PlayerData:
     def create_player(self, player):
         with open(self.file_name, 'a', newline='', encoding = 'utf-8') as csvfiles:
             fieldnames = ['name', 'id', 'address', 'gsm', 'phone', 'email', 'role', 'team']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer = csv.DictWriter(csvfiles, fieldnames=fieldnames)
 
             writer.writerow({'name': player.name, 'id': player.id, 'address': player.address, 'gsm': player.gsm, 'phone': player.phone, 'email': player.email, 'role': player.role, 'team': player.team})
 
