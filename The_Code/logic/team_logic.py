3ee6de0f635bc83d6a85e5d72140cfff00d966a8
joninam <118810664+joninam@users.noreply.github.com>
2022-11-30 12:03:28 +0000

    Update team_logic.py

--- The_Code/logic/team_logic.py
+++ The_Code/logic/team_logic.py
@@ -1,1 +1,12 @@
+from data.team_data import TeamData
+from model.team import Team
 
+class TeamLogic:
+    def __init__(self, team_connection):
+        self.data_wrapper = team_connection
+
+    def create_team(self, team):
+        self.data_wrapper.create_team(team)
+    
+    def list_all_teams(self):
+        return self.data_wrapper.list_all_teams()
