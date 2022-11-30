58b28e0badf7464c7af411d0a439d00dba11591d
joninam <118810664+joninam@users.noreply.github.com>
2022-11-30 12:06:32 +0000

    Update player_ui.py

--- The_Code/ui/player_ui.py
+++ The_Code/ui/player_ui.py
@@ -1,4 +1,4 @@
-from logic.player_logic import player_logic
+from logic.player_logic import PlayerLogic
 from model.player import Player 
 from ui.input_validators import *
 
@@ -21,7 +21,20 @@ class Player_UI:
                 print('going back')
                 return 'b'
             elif command == '1':
-                pass
+                p = Player()
+                while True:
+                    p.name = input('Enter the name of the player: ')
+                    p.player_id = input('Enter the player id: ')
+                    p.address = input('Enter the address of the player: ')
+                    p.email = input('Enter the email of the player: ')
+                    p.gsm = input('Enter the mobile phone number of the player: ')
+                    p.phone = input('Enter the phone number of the player: ')
+                    p.role = input('Enter the role of the player: ')
+                    p.team = input('Enter the name of the players team: ')
             elif command == '2':
-                pass
+                result = self.logic_wrapper.list_all_players()
+                for elem in result:
+                    print(f'name: {elem.name}, id: {elem.player_id}, address: {elem.address}, email: {elem.email}, mobile phone: {elem.gsm}, phone: {elem.phone}, role: {elem.role}, team: {elem.team}')
+            else: 
+                print('invalid input, try again')
            
