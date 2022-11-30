89286c5177deb9ce61d198952638bbaecd41f8f2
alexiaaxels <112617700+alexiaaxels@users.noreply.github.com>
2022-11-30 09:57:16 +0000

    Update club_logic.py

--- The_Code/logic/club_logic.py
+++ The_Code/logic/club_logic.py
@@ -3,7 +3,7 @@ from model.club import Club
 
 class PlayerLogic:
     def __init__(self, data_connection):
-        self.data_wrapper = data_connection
+        self.club_logic = data_connection
 
     def create_club(self, club):
         self.club_logic.create_club(club)
