f6b93e24f05cca0c30f9afacdbd9af386167a284
joninam <118810664+joninam@users.noreply.github.com>
2022-11-30 12:01:49 +0000

    Update player.py

--- The_Code/model/player.py
+++ The_Code/model/player.py
@@ -3,13 +3,13 @@ from model.team import Team
 class Player():
    def __init__(self, name="", player_id=0, address="", email="", gsm=0, phone=0, role="", team=""):
        self.name = name
-       self.player_id = id
+       self.player_id = player_id
        self.address = address
        self.email = email
        self.gsm = gsm
        self.phone = phone
        self.role = role
-       self.team = Team().id()
+       self.team = Team(self.name, self.team).id
   
    def __str__(self):
        return f"{self.name}, {self.player_id}, {self.address}, {self.email}, {self.gsm}, {self.phone}, {self.role}, {self.team}"
