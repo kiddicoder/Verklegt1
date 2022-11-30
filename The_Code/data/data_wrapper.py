1466531740659d3b794eef3ebb28f4c13ddb6f4b
joninam <118810664+joninam@users.noreply.github.com>
2022-11-30 12:04:59 +0000

    Update data_wrapper.py

--- The_Code/data/data_wrapper.py
+++ The_Code/data/data_wrapper.py
@@ -1,12 +1,16 @@
-from data.player_data import Player_Data
+from data.player_data import PlayerData
+from data.tournament_data import TournamentData
 
-
-class Data_Wrapper:
+class DataWrapper:
     def __init__(self):
-        self.player_data = Player_Data()
+        self.player_data = PlayerData()
+        self.tournament_data = TournamentData()
 
     def get_all_players(self):
         return self.player_data.read_all_players()
 
     def create_player(self, player):
         return self.player_data.create_player(player)
+
+    def create_tournament(self, tournament):
+        return self.tournament_data.create_tournament(tournament)
