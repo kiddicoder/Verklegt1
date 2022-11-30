c42a1cb9f960b228b141a0bd6cd52dc761a6ccbd
joninam <118810664+joninam@users.noreply.github.com>
2022-11-30 12:05:23 +0000

    Update team.py

--- The_Code/model/team.py
+++ The_Code/model/team.py
@@ -1,9 +1,11 @@
 from model.player import Player
+import uuid
 
 class Team():
     def __init__(self, player="", team_name=""): 
-        self.player = Player().id()    
+        self.player = player
         self.team_name = team_name
+        self.id = uuid.uuid4()
 
     def __str__(self):
         return f"{self.player}, {self.team_name}"
