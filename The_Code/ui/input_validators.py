92d7f5d30030465623cc602025332b19cc328db8
joninam <118810664+joninam@users.noreply.github.com>
2022-11-30 12:09:01 +0000

    Update input_validators.py

--- The_Code/ui/input_validators.py
+++ The_Code/ui/input_validators.py
@@ -1,1 +1,24 @@
+class NameLengthException(Exception):
+    pass
 
+def name_validator(name):
+    if len(name) >= 50:
+        raise NameLengthException()
+
+def id_validator(id):
+    if len(id) > 10 and id.isdigit() == False:
+        return ""
+
+def address_validator(address):
+    if len(address) >= 25:
+        raise NameLengthException()
+
+def phone_validator(phone): #á líka við gsm
+    if len(phone) > 7 and phone.isdigit() == False:
+        pass 
+
+def email_validator(email):
+    pass
+
+def role_validator(role):
+    pass
